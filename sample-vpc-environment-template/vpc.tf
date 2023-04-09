d6a8c54525f9cfb65a0b7448e0c9c7a836a10b6b
sbx_user1051 <sbx_user1051@169.254.196.33>
2023-04-09 09:47:27 +0000

    Update requested for Proton - environment/sample-vpc-environment-template

--- sample-vpc-environment-template/vpc.tf
+++ sample-vpc-environment-template/vpc.tf
@@ -6,13 +6,18 @@ To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:
 If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
 */
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.environment.inputs.vpc_name
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.aws_region}b"]
+  azs = [data.aws_availability_zones.available.names[0]]
+
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
