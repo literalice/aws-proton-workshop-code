8d76cc7424a1323b4053a88b50ca62057ee0a9a2
Masatoshi Hayashi <m@literalice.com>
2023-04-09 18:34:40 +0900

    available az

--- lambda-vpc/sample-vpc-environment-template/v1/infrastructure/vpc.tf
+++ lambda-vpc/sample-vpc-environment-template/v1/infrastructure/vpc.tf
@@ -1,10 +1,15 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.environment.inputs.vpc_name
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.aws_region}b"]
+  azs             = [data.aws_availability_zones.available.names[0]]
+  
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
