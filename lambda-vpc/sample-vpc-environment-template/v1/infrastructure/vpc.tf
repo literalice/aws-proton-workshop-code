ac6303694aab72d88ed1e736702d31b990df7d96
Masatoshi Hayashi <m@literalice.com>
2023-04-09 18:36:27 +0900

    gitignore

--- lambda-vpc/sample-vpc-environment-template/v1/infrastructure/vpc.tf
+++ lambda-vpc/sample-vpc-environment-template/v1/infrastructure/vpc.tf
@@ -8,8 +8,8 @@ module "vpc" {
   name = var.environment.inputs.vpc_name
   cidr = "10.0.0.0/16"
 
-  azs             = [data.aws_availability_zones.available.names[0]]
-  
+  azs = [data.aws_availability_zones.available.names[0]]
+
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
