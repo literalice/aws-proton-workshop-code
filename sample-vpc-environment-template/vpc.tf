f8eaa5072b2afbda19f888fe7d8fa24ebbacd6c7
sbx_user1051 <sbx_user1051@169.254.7.173>
2024-04-02 08:03:16 +0000

    Update requested for Proton - environment/sample-vpc-environment-template

---
+++ sample-vpc-environment-template/vpc.tf
@@ -0,0 +1,34 @@
+/*
+This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
+*/
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.environment.inputs.vpc_name
+  cidr = "10.0.0.0/16"
+
+  azs = [data.aws_availability_zones.available.names[0]]
+
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway                             = true
+  enable_vpn_gateway                             = false
+  enable_ipv6                                    = true
+  private_subnet_assign_ipv6_address_on_creation = false
+  public_subnet_ipv6_prefixes                    = [0]
+  private_subnet_ipv6_prefixes                   = [1]
+
+  tags = {
+    Terraform    = "true"
+    Environment  = var.environment.name
+    Provisioning = "via-proton"
+    Owner        = "Demo-EEM-TFC"
+  }
+}
+
